d0ae1a9e7d5bceb22d8a4503fecfd75338e68a0b
morsalin-islam335 <weshallovercome335@gmail.com>
2024-01-14 18:36:39 +0600

    blog-project-3

--- Cokie/project/firstApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import home,getCookie, deleteCookie
-
-urlpatterns = [
-    path("",home, name = 'homepage'),
-    path("get/",getCookie, name = 'getCookies'),
-    path("del/",deleteCookie, name = 'deleteCookies'),
-]
